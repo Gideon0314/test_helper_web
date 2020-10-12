298f1e71ac4068be20b20576e256c5fc6ec2c744
Gideon0314 <923198284@qq.com>
2020-10-12 16:55:59 +0800

    完善登陆功能

---
+++ src/api/user.js
@@ -0,0 +1,23 @@
+import request from '@/utils/request'
+
+export function login(params) {
+  return request({
+    url: '/user/login',
+    method: 'post',
+    auth: params
+  })
+}
+
+export function getInfo() {
+  return request({
+    url: '/user/info',
+    method: 'get',
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/logout',
+    method: 'post'
+  })
+}
---
+++ src/utils/request.js
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import { MessageBox, Message } from 'element-ui'
+import store from '@/store'
+import { getToken } from '@/utils/auth'
+
+// create an axios instance
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000 // request timeout
+})
+
+// request interceptor
+service.interceptors.request.use(
+config => {
+    // 如果存在 token 则附带在 http header 中
+    if (store.getters.token) {
+      config.headers['Authorization'] = `Bearer ${getToken()}`
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+service.interceptors.response.use(
+  response => {
+    const res = response.data
+
+    if (res.status !== 200) {
+      Message({
+        message: res.msg || 'Error',
+        type: 'error',
+        duration: 5 * 1000
+      })
+      // 判断 token 失效的场景
+      if (res.status === -2) {
+        // 如果 token 失效，则弹出确认对话框，用户点击后，清空 token 并返回登录页面
+        MessageBox.confirm('Token 失效，请重新登录', '确认退出登录', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        })
+      }
+      return Promise.reject(new Error(res.msg || '请求失败'))
+    } else {
+      return res
+    }
+  },
+  error => {
+    let message = error.message || '请求失败'
+    if (error.response && error.response.data) {
+      const { data } = error.response
+      message = data.msg
+    }
+    Message({
+      message,
+      type: 'error',
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  },
+
+)
+
+export default service
