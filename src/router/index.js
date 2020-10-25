7c9d0d10683a9b2c693e9b97b1e75dc5be1cad9d
923198284@qq.com <923198284@qq.com>
2020-10-25 19:16:26 +0800

    新增测试任务路由

--- src/router/index.js
+++ src/router/index.js
@@ -107,7 +107,9 @@ export const constantRoutes = [
       },
       {
         path: '/api-test/task/board',
-        component: () => import('@/views/api-test/task-board')
+        component: () => import('@/views/api-test/task-board'),
+        meta: { title: 'editArticle', noCache: true, activeMenu: '/api-test/task' },
+        hidden: true
       }
     ]
   }
